96f57f14bc19c2ace20fcb38bdf3f4ff37304c7d
suchitrasahu996-creator <suchitrasahu996@gmail.com>
2025-12-28 19:38:09 +0530

    adding the answer

---
+++ mini-fleet-management-app/src/page/AdminDashbord.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+
+function AdminDashbord() {
+const [fleets]
+
+
+
+
+  return (
+    <div>
+        <h2>Admin Dashboard</h2>
+        <div>
+            <h3>Add Fleet</h3>
+            <input name="RegNo" placeholder="Vechicle Reg No" value={Form.RegNo} onChange={handleChange}/>
+            <selec>
+                <option value="">Selecte from the below Category</option>
+                <option>Auto</option>
+                <option>Car</option>
+                <option>Truck</option>
+                <option>Bus</option>
+            </selec>
+            <input name="Driver" placeholder="Driver Name" value={Form.Driver} onChange={handleChange}/>
+            <select name="Status" value={Form.Status} onChange={handleChange}>
+                <option>Available</option>
+                <option>Unavailable</option>
+
+            </select>
+            <button onClick={addFleet}>ADD FLEET</button>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default AdminDashbord
